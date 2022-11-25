1ba9312eb55e1bb8190ee2d002dc0f2f3fb5530a
linux-printer-troubleshooter <117502862+linux-printer-troubleshooter@users.noreply.github.com>
2022-11-25 11:59:10 +0530

    Update wifi_.sh

--- wifi_.sh
+++ wifi_.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #add a title if you wish like your_title="my custom title"
 your_title=""
+#add a custom header if you so desire
+header=""
 
 #grep wifi and remove a device called wifi-p2p
 name=$(nmcli device show |  grep -v wifi[-] | grep -B 1 wifi | grep -i general.device | awk '{print $2}');
@@ -11,9 +13,9 @@ status=$(nmcli device show $name | grep GENERAL.STATE | awk '{print $2}');
 #if loop changes appearance of off and on toggle
 if [[ $status -eq 20 ]]
 then
-arg=$(zenity --forms --title="$your_title" --text="Nmcli Dialog\n On or off" --add-list="$name" --list-values="Off|On" --column-values="Select");
+arg=$(zenity --forms --title="$your_title" --text="$header" --add-list="$name" --list-values="Off|On" --column-values="Select");
 else
-arg=$(zenity --forms  --title="$your_title" --text="Nmcli Dialog\n On or off" --add-list="$name" --list-values="On|Off" --column-values="Select");
+arg=$(zenity --forms  --title="$your_title" --text="$header" --add-list="$name" --list-values="On|Off" --column-values="Select");
 fi;
 
 #confirms selected action
