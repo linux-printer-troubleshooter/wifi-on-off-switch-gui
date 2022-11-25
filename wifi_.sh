153637994b5d54561bf106eba6264a1e90c7b77f
linux-printer-troubleshooter <117502862+linux-printer-troubleshooter@users.noreply.github.com>
2022-11-25 11:37:42 +0530

    Update wifi_.sh

--- wifi_.sh
+++ wifi_.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+#grep wifi and remove a device called wifi-p2p
 name=$(nmcli device show |  grep -v wifi[-] | grep -B 1 wifi | grep -i general.device | awk '{print $2}');
+#grep general.state=20 which means Unavailable (radio off)
 status=$(nmcli device show $name | grep GENERAL.STATE | awk '{print $2}');
 if [[ $status -eq 20 ]]
 then
