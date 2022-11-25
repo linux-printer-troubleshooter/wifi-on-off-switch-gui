4f810e57d9046b4cabaf328c6070b61aa8dec0c6
linux-printer-troubleshooter <117502862+linux-printer-troubleshooter@users.noreply.github.com>
2022-11-25 11:41:58 +0530

    Update wifi_.sh

--- wifi_.sh
+++ wifi_.sh
@@ -3,6 +3,7 @@
 name=$(nmcli device show |  grep -v wifi[-] | grep -B 1 wifi | grep -i general.device | awk '{print $2}');
 #grep general.state=20 which means Unavailable (radio off)
 status=$(nmcli device show $name | grep GENERAL.STATE | awk '{print $2}');
+#if loop changes appearance of off and on toggle
 if [[ $status -eq 20 ]]
 then
 arg=$(zenity --forms --add-list="$name" --list-values="Off|On" --column-values="Select");
@@ -10,5 +11,5 @@ else
 arg=$(zenity --forms --add-list="$name" --list-values="On|Off" --column-values="Select");
 fi;
 
-
+#confirms selected action
 if [[ $arg == "On" ]];then nmcli radio wifi     on; fi; if [[ $arg == "Off" ]];then nmcli radio wifi off;fi
